58095c9c476263de0a37dc900edccad0e6e64331
Anna Moura Sobral <annamourasobral@gmail.com>
2023-05-18 15:24:50 +0200

    github oauth setup

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export default function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Made with 💜 by{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://github.com/annamourasobral"
+      >
+        Anna Moura Sobral
+      </a>
+    </div>
+  )
+}
